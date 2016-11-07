594ed05fff36e7177a33297f74920151ac486527
Mohamed Anas <mabo.tn@outlook.com>
2016-11-07 01:09:52 +0100

    Updated Project Structure, Added Views, Business and Data Access files of Enterprise

---
+++ stage_isetna/DataAccess/EntreprisesDA.cs
@@ -0,0 +1,37 @@
+﻿using stage_isetna.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stage_isetna.DataAccess
+{
+    class EntreprisesDA
+    {
+        public static List<Entreprise> Retrive ()
+        {
+            return new List<Entreprise>();
+        }
+
+        public static Entreprise Retrive(string condition)
+        {
+            return new Entreprise();
+        }
+
+        public static bool Create(Entreprise entreprise)
+        {
+            return true;
+        }
+
+        public static bool Update(Entreprise entreprise)
+        {
+            return true;
+        }
+
+        public static bool Delete(Entreprise entreprise)
+        {
+            return true;
+        }
+    }
+}
