04503684aaefaa6cda80d9514c624ab531722fd1
HabibAroua <habibha.aroua82@gmail.com>
2016-11-09 19:25:56 +0100

    Modification in EntrepriseDAO

--- stage_isetna/Entreprise.cs
+++ stage_isetna/Entreprise.cs
@@ -16,27 +16,5 @@ namespace stage_isetna
         {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            stage_isetna.DataAccess.EntrepriseDA en = new stage_isetna.DataAccess.EntrepriseDA();
-            List<stage_isetna.Business.Entreprise> l=en.Retrive();
-            foreach(stage_isetna.Business.Entreprise el in l)
-            {
-                MessageBox.Show(el.getId()+" "+ el.getnomEntreprise()+" "+el.getTelephone());
-                label1.Text = el.getId() + " " + el.getnomEntreprise() + " " + el.getTelephone();
-            }
-            //stage_isetna.Business.Entreprise en = new stage_isetna.Business.Entreprise(2, "Igarashi", "Japan", "71596");
-            //stage_isetna.DataAccess.EntrepriseDA eDA = new stage_isetna.DataAccess.EntrepriseDA();
-            //Boolean test= eDA.Update(2,en);
-            //if(test==true)
-            //{
-              // MessageBox.Show("Insertion effectué avec  succes");
-            //}
-            //else
-            //{
-                //MessageBox.Show("Insertion echoué");
-            //}
-        }
     }
 }
