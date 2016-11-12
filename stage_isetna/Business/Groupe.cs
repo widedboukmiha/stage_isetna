51efe59b5ac26a1de0c9ea2f8d6239110353f290
SafaMiri <mirisafa1@gmail.com>
2016-11-12 11:52:31 +0100

    j'ai developpée les Crud de groupe

---
+++ stage_isetna/Business/Groupe.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stage_isetna.Business
+{
+    class Groupe
+    {
+        private String id_Groupe;
+        private String nom_Groupe;
+
+        public Groupe(String id_Groupe, String nom_Groupe)
+        {
+            this.id_Groupe = id_Groupe;
+            this.nom_Groupe = nom_Groupe;
+
+        }
+
+        public String getId_Groupe()
+        {
+            return id_Groupe;
+        }
+        public String getNom_Groupe()
+        {
+            return nom_Groupe;
+        }
+
+        public void setId_Groupe(String id_Groupe)
+        {
+            this.id_Groupe = id_Groupe;
+        }
+
+        public void setNom_Groupe(String nom_Groupe)
+        {
+            this.nom_Groupe = nom_Groupe;
+        }
+
+        public String toString()
+        {
+            return "id_Groupe=" + id_Groupe + ", nom_Groupe=" + nom_Groupe;
+        }
+
+
+    }
+}
