1f0e5f8c158d99ab17aae8b3edd9255b1c8415bf
Mohamed Anas <mabo.tn@outlook.com>
2016-11-15 00:26:30 +0100

    Added NiveauDA

---
+++ stage_isetna/DataAccess/GroupDA.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace stage_isetna.DataAccess
+{
+    class GroupDA
+    {
+        private string conString = "";
+        public void Create(Business.Group group)
+        {
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                con.Open();
+                using (SqlCommand cmd = con.CreateCommand())
+                {
+                    cmd.CommandText = String.Format("INSERT INTO Group VALUES (NULL, '{0}')", group.Nom);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public List<Business.Group> Get()
+        {
+            DataSet ds = new DataSet();
+            using (SqlCommand cmd = new SqlCommand("SELECT * FROM Group", new SqlConnection(conString)))
+            {
+                cmd.Connection.Open();
+                DataTable table = new DataTable();
+                table.Load(cmd.ExecuteReader());
+                ds.Tables.Add(table);
+            }
+
+            var list = ds.Tables[0].AsEnumerable().Select(dataRow => new Business.Group { Id = dataRow.Field<int>("Id"), Nom = dataRow.Field<string>("Nom") }).ToList();
+            return list;
+        }
+
+        public Business.Group Get(int id)
+        {
+            DataSet ds = new DataSet();
+            using (SqlCommand cmd = new SqlCommand("SELECT * FROM Group WHERE Id = " + id.ToString(), new SqlConnection(conString)))
+            {
+                cmd.Connection.Open();
+                DataTable table = new DataTable();
+                table.Load(cmd.ExecuteReader());
+                ds.Tables.Add(table);
+            }
+
+            var list = ds.Tables[0].AsEnumerable().Select(dataRow => new Business.Group { Id = dataRow.Field<int>("Id"), Nom = dataRow.Field<string>("Nom") }).ToList();
+            return list[0];
+        }
+
+        public void Update(int id, Business.Group group)
+        {
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                con.Open();
+                using (SqlCommand cmd = con.CreateCommand())
+                {
+                    cmd.CommandText = String.Format("UPDATE Group SET Nom = '{0}' WHERE Id = {1}", group.Nom, id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                con.Open();
+                using (SqlCommand cmd = con.CreateCommand())
+                {
+                    cmd.CommandText = String.Format("DELETE FROM Group WHERE Id = {0})", id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
