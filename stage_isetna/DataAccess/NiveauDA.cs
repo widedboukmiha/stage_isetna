8b7bcf35fb2ec5328a8828976d0ad25d7522c73a
Safa Bettaieb <safa.bettaieb1@gmail.com>
2016-11-15 00:31:13 +0100

    Updated NiveauDA

--- stage_isetna/DataAccess/NiveauDA.cs
+++ stage_isetna/DataAccess/NiveauDA.cs
@@ -1,5 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,74 @@ namespace stage_isetna.DataAccess
 {
     class NiveauDA
     {
+        private string conString = "";
+        public void Create(Business.Niveau niveau)
+        {
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                con.Open();
+                using (SqlCommand cmd = con.CreateCommand())
+                {
+                    cmd.CommandText = String.Format("INSERT INTO Niveau VALUES (NULL, '{0}')", niveau.Nom);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public Business.Niveau Get()
+        {
+            DataSet ds = new DataSet();
+            using (SqlCommand cmd = new SqlCommand("SELECT * FROM Niveau", new SqlConnection(conString)))
+            {
+                cmd.Connection.Open();
+                DataTable table = new DataTable();
+                table.Load(cmd.ExecuteReader());
+                ds.Tables.Add(table);
+            }
+
+            var list = ds.Tables[0].AsEnumerable().Select(dataRow => new Business.Niveau { Id = dataRow.Field<int>("Id"), Nom = dataRow.Field<string>("Nom") }).ToList();
+            return list;
+        }
+
+        public Business.Niveau Get(int id)
+        {
+            DataSet ds = new DataSet();
+            using (SqlCommand cmd = new SqlCommand("SELECT * FROM Niveau WHERE Id = " + id.ToString(), new SqlConnection(conString)))
+            {
+                cmd.Connection.Open();
+                DataTable table = new DataTable();
+                table.Load(cmd.ExecuteReader());
+                ds.Tables.Add(table);
+            }
+
+            var list = ds.Tables[0].AsEnumerable().Select(dataRow => new Business.Niveau { Id = dataRow.Field<int>("Id"), Nom = dataRow.Field<string>("Nom") }).ToList();
+            return list[0];
+        }
+
+        public void Update(int id, Business.Niveau niveau)
+        {
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                con.Open();
+                using (SqlCommand cmd = con.CreateCommand())
+                {
+                    cmd.CommandText = String.Format("UPDATE Niveau SET Nom = '{0}' WHERE Id = {1}", niveau.Nom, id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                con.Open();
+                using (SqlCommand cmd = con.CreateCommand())
+                {
+                    cmd.CommandText = String.Format("DELETE FROM Niveau WHERE Id = {0})", id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
     }
 }
