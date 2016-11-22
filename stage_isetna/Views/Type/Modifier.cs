e0d2f3a5332ffa30f39b46ce18fcc8c1d4f74fbd
Mohamed Anas <mabo.tn@outlook.com>
2016-11-22 05:11:31 +0100

    Added [Ajouter, Modifier, Supprimer] for Type

---
+++ stage_isetna/Views/Type/Modifier.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace stage_isetna.Views.Type
+{
+    public partial class Modifier : Form
+    {
+        private int Id;
+
+        public Modifier(int id)
+        {
+            InitializeComponent();
+            Id = id;
+        }
+
+        private void btnAjout_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                new DataAccess.TypeDA().Update(Id, txtNom.Text);
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            new Supprimer(Id).ShowDialog();
+            this.Close();
+        }
+    }
+}
