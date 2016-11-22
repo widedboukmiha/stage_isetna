357f14fc0cc7ff9d82c61ae727df6aa64f34182d
Safa Bettaieb <safa.bettaieb1@gmail.com>
2016-11-22 05:23:20 +0100

    Modification sur EntrepriseDA

--- stage_isetna/DataAccess/EntrepriseDA.cs
+++ stage_isetna/DataAccess/EntrepriseDA.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,12 +10,24 @@ namespace stage_isetna.DataAccess
 {
     class EntrepriseDA
     {
-        //SqlConnection conString = new SqlConnection(Properties.Settings.Default.chaineHabib);
+        //private static string conString = Properties.Settings.Default.chaine;
+        //private static string conString = Properties.Settings.Default.chaineHabib;
+        //private static string conString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\wided boukmiha\\Documents\\GitHub\\stage_isetna\\stage_isetna\\stage_isetna\\stage_isetna\\Database\\Database.mdf;Integrated Security=True";
         private static string conString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\stage_isetna\\stage_isetna\\Database\\Database.mdf;Integrated Security=True";
-        public EntrepriseDA()
-        {
 
+        public static void Create(string Nom, string Adresse, string Ville, string NumTel)
+        {
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                con.Open();
+                using (SqlCommand cmd = con.CreateCommand())
+                {
+                    cmd.CommandText = String.Format("INSERT INTO [Entreprise] VALUES ((SELECT MAX(Id) + 1 FROM [Entreprise]), '{0}', '{1}', '{2}', '{3}')", Nom, Adresse, Ville, NumTel);
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
+
         public static List<Business.Entreprise> Get()
         {
             DataSet ds = new DataSet();
@@ -34,118 +46,103 @@ namespace stage_isetna.DataAccess
                 Ville = dataRow.Field<string>("Ville"),
                 NumTel = dataRow.Field<string>("NumTel")
             }).ToList();
+
             return list;
         }
 
-        private static int afficheMax()
+        public Business.Entreprise Get(int Id)
         {
-            int r = 0;
-            SqlConnection cn = new SqlConnection(Properties.Settings.Default.chaineHabib); // ya jmé3a lazim t7otou chaine de connexion imté3kom fi el blassa hadhi 
-            SqlCommand cmd = cn.CreateCommand();
-            cmd.CommandText = "select Max(id) from Entreprise";
-            cn.Open();
-            r = Convert.ToInt32(cmd.ExecuteScalar());
-            cn.Close();
-            return r;
+            DataSet ds = new DataSet();
+            using (SqlCommand cmd = new SqlCommand("SELECT * FROM [Entreprise] WHERE Id = " + Id.ToString(), new SqlConnection(conString)))
+            {
+                cmd.Connection.Open();
+                DataTable table = new DataTable();
+                table.Load(cmd.ExecuteReader());
+                ds.Tables.Add(table);
+            }
+
+            var list = ds.Tables[0].AsEnumerable().Select(dataRow => new Business.Entreprise
+            {
+                Id = dataRow.Field<int>("Id"),
+                Nom = dataRow.Field<string>("Nom"),
+                Adresse = dataRow.Field<string>("Adresse"),
+                Ville = dataRow.Field<string>("Ville"),
+                NumTel = dataRow.Field<string>("NumTel")
+            }).ToList();
+
+            return list[0];
+        }
+        public Business.Entreprise Get(string Nom)
+        {
+            DataSet ds = new DataSet();
+            using (SqlCommand cmd = new SqlCommand("SELECT * FROM [Entreprise] WHERE Nom = '" + Nom + "'", new SqlConnection(conString)))
+            {
+                cmd.Connection.Open();
+                DataTable table = new DataTable();
+                table.Load(cmd.ExecuteReader());
+                ds.Tables.Add(table);
+            }
+
+
+            var list = ds.Tables[0].AsEnumerable().Select(dataRow => new Business.Entreprise
+            {
+                Id = dataRow.Field<int>("Id"),
+                Nom = dataRow.Field<string>("Nom"),
+                Adresse = dataRow.Field<string>("Adresse"),
+                Ville = dataRow.Field<string>("Ville"),
+                NumTel = dataRow.Field<string>("NumTel")
+            }).ToList();
+
+            return list[0];
         }
 
-        public static bool Create(Business.Entreprise e)
+        public static void Update(int Id, string Nom, string Adresse, string Ville, string NumTel)
         {
-            try
+            using (SqlConnection con = new SqlConnection(conString))
             {
-                using (SqlConnection con = new SqlConnection(conString))
+                con.Open();
+                using (SqlCommand cmd = con.CreateCommand())
                 {
-                    con.Open();
-                    using (SqlCommand cmd = con.CreateCommand())
-                    {
-                        cmd.CommandText = String.Format("INSERT INTO [Entreprise] VALUES ((SELECT MAX(Id) + 1 FROM [Group]), '{0}', '{1}', '{2}', '{3}')", e.Nom, e.Adresse, e.Ville, e.NumTel);
-                        cmd.ExecuteNonQuery();
-
-                        return true;
-                    }
+                    cmd.CommandText = String.Format("UPDATE [Entreprise] SET Nom = '{1}', Adresse = '{2}', Ville = '{3}', NumTel = '{4}' WHERE Id = {0}", Id, Nom, Adresse, Ville, NumTel);
+                    cmd.ExecuteNonQuery();
                 }
             }
-            catch (Exception)
+        }
+
+        public static void Delete(int Id)
+        {
+            using (SqlConnection con = new SqlConnection(conString))
             {
-                return false;
+                con.Open();
+                using (SqlCommand cmd = con.CreateCommand())
+                {
+                    cmd.CommandText = String.Format("DELETE FROM [Entreprise] WHERE Id = {0}", Id);
+                    cmd.ExecuteNonQuery();
+                }
             }
         }
 
-        public Boolean Update(int id, stage_isetna.Business.Entreprise en)
+        public static List<Business.Entreprise> Find(string Nom, string Adresse, string Ville, string NumTel)
         {
+            DataSet ds = new DataSet();
+            using (SqlCommand cmd = new SqlCommand("SELECT * FROM [Entreprise] WHERE Nom LIKE '%" + Nom + "%' OR Adresse LIKE '%" + Adresse + "%' OR Ville LIKE '%" + Ville + "%' OR NumTel LIKE '%" + NumTel + "%'", new SqlConnection(conString)))
+            {
+                cmd.Connection.Open();
+                DataTable table = new DataTable();
+                table.Load(cmd.ExecuteReader());
+                ds.Tables.Add(table);
+            }
 
-            try
+            var list = ds.Tables[0].AsEnumerable().Select(dataRow => new Business.Entreprise
             {
-                SqlConnection conString = new SqlConnection(EntrepriseDA.conString);
-                conString.Open();
-                SqlCommand cmd = new SqlCommand("UPDATE Entreprise set nomEntreprise='" + en.Nom + "'," + "telephone='" + en.NumTel + "',adresse='" + en.Adresse + "' where id=" + id, conString);
-                SqlDataReader Reader = cmd.ExecuteReader();
-                conString.Close();
-                return true;
-            }
-            catch (Exception ex)
-            {
-                String er = ex.Message;
-                return false;
-            }
-        }
+                Id = dataRow.Field<int>("Id"),
+                Nom = dataRow.Field<string>("Nom"),
+                Adresse = dataRow.Field<string>("Adresse"),
+                Ville = dataRow.Field<string>("Ville"),
+                NumTel = dataRow.Field<string>("NumTel")
+            }).ToList();
 
-        public Boolean Remove(int id)
-        {
-            SqlCommand cmd = new SqlCommand();
-            SqlConnection connexion = new SqlConnection();
-            connexion.ConnectionString = Properties.Settings.Default.chaineHabib;
-            try
-            {
-                string req = string.Format("DELETE FROM Entreprise WHERE id = " + id);
-                cmd.Connection = connexion;
-                connexion.Open();
-                cmd.CommandText = req;
-                cmd.ExecuteNonQuery();
-                connexion.Close();
-                return true;
-            }
-            catch (Exception ex)
-            {
-                return false;
-            }
-        }
-
-        public void afficheGrid(DataGridView v)
-        {
-            try
-            {
-                SqlDataAdapter adap1;
-                DataTable tab1;
-                adap1 = new SqlDataAdapter("select Nom,Adresse,Ville,numTel from Entreprise ", conString);
-                DataSet dtst = new DataSet();
-                adap1.Fill(dtst, "Entreprise");
-                tab1 = dtst.Tables["Entreprise"];
-                v.DataSource = tab1;
-            }
-            catch (SqlException ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
-        public void searchGrid(DataGridView v, string value)
-        {
-            try
-            {
-                SqlDataAdapter adap1;
-                DataTable tab1;
-                adap1 = new SqlDataAdapter("select Nom,Adresse,Ville,numTel from Entreprise where nom like'" + value + "%'", conString);
-                DataSet dtst = new DataSet();
-                adap1.Fill(dtst, "Entreprise");
-                tab1 = dtst.Tables["Entreprise"];
-                v.DataSource = tab1;
-            }
-            catch (SqlException ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+            return list;
         }
     }
 }
-
